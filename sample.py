b6d2dd1a6b42392316699a56c54dcee53ee73729
praseo <praseodeveloper@gmail.com>
2025-04-06 23:14:33 +0200

    Function calling with natural-functions model

--- sample.py
+++ sample.py
@@ -1,156 +1,128 @@
-#!/usr/bin/env python3
-"""
-Ollama with functions
-"""
-import inspect
 import json
-from typing import get_type_hints
+import ollama
+import asyncio
 
-import requests
-import rich
-from rich import print_json
-
-
-def generate_full_completion(model: str, prompt: str, **kwargs) -> dict[str, str]:
-    params = {"model": model, "prompt": prompt, "stream": False}
-    try:
-        response = requests.post(
-            f"http://localhost:11434/api/generate",
-            headers={"Content-Type": "application/json"},
-            data=json.dumps(params),
-            timeout=60,
-        )
-        # print(f"🤖 Request: {json.dumps(params)} -> Response: {response.text}")
-        response.raise_for_status()
-        return json.loads(response.text)
-    except requests.RequestException as err:
-        return {"error": f"API call error: {str(err)}"}
-
-
-class Article:
-    pass
-
-
-class Weather:
-    pass
-
-
-class Directions:
-    pass
-
-
-def calculate_mortgage_payment(
-    loan_amount: int, interest_rate: float, loan_term: int
-) -> float:
-    """Get the monthly mortgage payment given an interest rate percentage."""
-
-    # TODO: you must implement this to actually call it later
-
-
-def get_article_details(
-    title: str,
-    authors: list[str],
-    short_summary: str,
-    date_published: str,
-    tags: list[str],
-) -> Article:
-    '''Get article details from unstructured article text.
-    date_published: formatted as "MM/DD/YYYY"'''
-
-    # TODO: you must implement this to actually call it later
-
-
-def get_weather(city: str) -> Weather:
-    """Get the current weather given a city."""
-
-    # TODO: you must implement this to actually call it later
-
-
-def get_directions(start: str, destination: str) -> Directions:
-    """Get directions from Google Directions API.
-    start: start address as a string including zipcode (if any)
-    destination: end address as a string including zipcode (if any)"""
-
-    # TODO: you must implement this to actually call it later
-
-
-def get_type_name(t):
-    name = str(t)
-    if "list" in name or "dict" in name:
-        return name
-    else:
-        return t.__name__
-
-
-def function_to_json(func):
-    signature = inspect.signature(func)
-    type_hints = get_type_hints(func)
-
-    function_info = {
-        "name": func.__name__,
-        "description": func.__doc__,
-        "parameters": {"type": "object", "properties": {}},
-        "returns": type_hints.get("return", "void").__name__,
+def get_spouse_name(name: str) -> str:
+    "Get the name of a person's spouse"
+    names = {
+        "Ranveer": "Deepika",
+        "Aalia": "Ranbir",
+        "Raja": "Rani"
     }
+    return json.dumps(names.get(name.lower(), "NA"))
 
-    for name, _ in signature.parameters.items():
-        param_type = get_type_name(type_hints.get(name, type(None)))
-        function_info["parameters"]["properties"][name] = {"type": param_type}
+def order_pizza(arguments: object) -> str:
+    return ("A " + arguments["size"] + " pizza has been ordered")
 
-    return json.dumps(function_info, indent=2)
-
-
-def main():
-    functions_prompt = f"""
-You have access to the following tools:
-{function_to_json(get_weather)}
-{function_to_json(calculate_mortgage_payment)}
-{function_to_json(get_directions)}
-{function_to_json(get_article_details)}
-
-You must follow these instructions:
-Always select one or more of the above tools based on the user query
-If a tool is found, you must respond in the JSON format matching the following schema:
-{{
-   "tools": {{
-        "tool": "<name of the selected tool>",
-        "tool_input": <parameters for the selected tool, matching the tool's JSON schema
-   }}
-}}
-If there are multiple tools required, make sure a list of tools are returned in a JSON array.
-If there is no tool that match the user request, you will respond with empty json.
-Do not add any additional Notes or Explanations
-
-User Query:
-    """
-
-    GPT_MODEL = "mixtral"
-
-    prompts = [
-        "What's the weather in London, UK?",
-        "Determine the monthly mortgage payment for a loan amount of $200,000, an interest rate of 4%, and a loan term of 30 years.",
-        "What's the current exchange rate for GBP to EUR?",
-        "I'm planning a trip to Killington, Vermont (05751) from Hoboken, NJ (07030). Can you get me weather for both locations and directions?",
+async def run(model: str, user_input: str):
+    client = ollama.AsyncClient()
+    # Initialize conversation with a user query
+    messages = [
+        {
+            "role": "system",
+            "content": ' \
+                You are a helpful assistant with access to the following functions. Use them if required. \
+                If a function call returns "NA", then attempt answering the same question using your knowledge. \
+                {\
+                    "name": "order_pizza",\
+                    "description": "Order a pizza with custom toppings",\
+                    "parameters": {\
+                        "type": "object",\
+                        "properties": {\
+                            "size": {\
+                                "type": "string",\
+                                "description": "Size of the pizza (small, medium, large)"\
+                            },\
+                            "crust": {\
+                                "type": "string",\
+                                "description": "Type of crust (thin, regular, thick)"\
+                            },\
+                            "toppings": {\
+                                "type": "array",\
+                                "items": {\
+                                    "type": "string"\
+                                },\
+                                "description": "List of toppings for the pizza"\
+                            },\
+                            "delivery_address": {\
+                                "type": "string",\
+                                "description": "Address where the pizza should be delivered"\
+                            }\
+                        },\
+                        "required": [\
+                            "size",\
+                            "crust",\
+                            "toppings",\
+                            "delivery_address"\
+                        ]\
+                    }\
+                },  \
+                { \
+                    "name": "get_spouse_name", \
+                    "description": "Get the name of the spouse of a person", \
+                    "parameters": { \
+                        "type": "object", \
+                        "properties": { \
+                            "name": { \
+                                "type": "string", \
+                                "description": "The name of a person whose spouse needs to be identified.", \
+                            }, \
+                        },\
+                        "required": ["name"]\
+                    }\
+                }'
+        },
+        {
+            "role": "user",
+            "content": user_input
+        }
     ]
 
-    for prompt in prompts:
-        print(f"❓{prompt}")
-        question = functions_prompt + prompt
-        response = generate_full_completion(GPT_MODEL, question)
-        try:
-            tidy_response = (
-                response.get("response", response)
-                .strip()
-                .replace("\n", "")
-                .replace("\\", "")
+    # First API call: Send the query and function description to the model
+    response = await client.chat(
+        model=model,
+        messages=messages
+    )
+
+    # Add the model's response to the conversation history
+    messages.append(response["message"])
+
+    # Check if the model decided to use the provided function
+    response_content = response["message"].content
+    if "<functioncall>" in response_content:
+        print("Function called!")
+        function_obj = response_content[14:]
+        corrected_data = function_obj.replace("\'", "").replace("'", '"').replace('"{"', '{"').replace('"}"', '}')
+        function_def = json.loads(corrected_data)
+        if function_def["name"] == "order_pizza":
+            result = order_pizza(function_def["arguments"])
+            print(result)
+            messages.append(
+                {
+                    "role": "tool",
+                    "content": result,
+                }
             )
-            print_json(tidy_response)
-            rich.print(
-                f"[bold]Total duration: {int(response.get('total_duration')) / 1e9} seconds [/bold]"
-            )
-        except Exception:
-            print(f"❌ Unable to decode JSON. {response}")
+        elif function_def["name"] == "get_spouse_name": 
+            result = get_spouse_name(function_def["arguments"]["name"])
+            print(result)
+            messages.append({
+                "role": "tool",
+                "content": result
+            })
+        else: 
+            print("Unknown function call detected!")
+    else:
+        print("\nThe model didn't use the function. Its response was:")
+        print(response["message"]["content"])
+        return
 
 
-if __name__ == "__main__":
-    main()
+while True:
+    user_input = input("\n Please ask=> ")
+    if not user_input:
+        user_input = "What is the capital of India?"
+    if user_input.lower() == "/bye":
+        break
+
+    asyncio.run(run("calebfahlgren/natural-functions", user_input))
